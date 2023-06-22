b95fc2eedd46271164fa6ebd18bf4a5137f4e598
csehoferi891122 <cs.k.feri89@gmail.com>
2023-06-22 08:23:20 +0200

    add javadoc for rest controller

--- src/main/java/com/example/allianz/controller/EventController.java
+++ src/main/java/com/example/allianz/controller/EventController.java
@@ -10,6 +10,12 @@ import org.springframework.web.bind.annotation.*;
 import java.time.LocalDate;
 import java.util.List;
 
+/**
+ * Allianz test feladatok Rest interfész
+ *
+ * @author Ferenc Csehó-Kovács
+ *
+ */
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("event")
@@ -17,16 +23,31 @@ public class EventController {
 
     private final EventService eventService;
 
+    /**
+     * <p>Meeting létrehozása</p>
+     * @param createEvent Meeting létrehozásához szükséges paramtéereket tartalmazó record
+     * @exception EventTimeException
+     * @return létrehozott meeting
+     */
     @PutMapping
     public Event createEvent(@RequestBody CreateEvent createEvent) throws EventTimeException {
         return eventService.createEvent(createEvent);
     }
 
+    /**
+     * <p>Meetingek lekérdezése dátum alapján</p>
+     * @param date Meeting napja
+     * @return adott napi meetingek
+     */
     @GetMapping("/{date}")
     public List<Event> getEventsByDate(@PathVariable LocalDate date) {
         return eventService.getEventsByDate(date);
     }
 
+    /**
+     * <p>Addot heti meetingek lekérdezése</p>
+     * @return adott heti meetingek
+     */
     @GetMapping("/for-week")
     public List<Event> getEventsForWeek() {
         return eventService.getEventsForWeek();
